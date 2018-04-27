18860cbae7b26b3818a3b3391aa07cf969722d5e
ice1000 <ice1000kotlin@foxmail.com>
2018-04-27 22:16:46 +0800

    meow

--- setup.kts
+++ setup.kts
@@ -5,43 +5,43 @@ fun github(org: String, repo: String) = "https://github.com/$org/$repo.git"
 val target = File("target").apply { if (!exists()) mkdir() }
 
 listOf(
-		official("julia"),
-		official("json"),
-		official("solidity"),
-		official("emmylua"),
-		official("la-clojure"),
-		github("covscript", "covscript-devkt")
+    official("julia"),
+    official("json"),
+    official("solidity"),
+    official("emmylua"),
+    official("la-clojure"),
+    github("covscript", "covscript-devkt")
 )
-		.asSequence()
-		.onEach { repository ->
-			ProcessBuilder()
-					.command("git", "clone", repository)
-					.directory(null)
-					.inheritIO()
-					.start()
-					.waitFor()
-		}
-		.map { it.split('/') }
-		.mapNotNull { it.lastOrNull() }
-		.map { File(it) }
-		.onEach { project ->
-			ProcessBuilder()
-					.command("bash", "gradlew", "jar")
-					.directory(project)
-					.inheritIO()
-					.start()
-					.waitFor()
-		}
-		.map { it.resolve("build") }
-		.map { it.resolve("libs") }
-		.flatMap {
-			it
-					.listFiles()
-					.orEmpty()
-					.filterNotNull()
-					.asSequence()
-		}
-		.filter { it.extension == "jar" }
-		.forEach {
-			it.copyTo(target.resolve(it.name), overwrite = true)
-		}
+    .asSequence()
+    .onEach { repository ->
+      ProcessBuilder()
+          .command("git", "clone", repository)
+          .directory(null)
+          .inheritIO()
+          .start()
+          .waitFor()
+    }
+    .map { it.split('/') }
+    .mapNotNull { it.lastOrNull() }
+    .map { File(it) }
+    .onEach { project ->
+      ProcessBuilder()
+          .command("bash", "gradlew", "jar")
+          .directory(project)
+          .inheritIO()
+          .start()
+          .waitFor()
+    }
+    .map { it.resolve("build") }
+    .map { it.resolve("libs") }
+    .flatMap {
+      it
+          .listFiles()
+          .orEmpty()
+          .filterNotNull()
+          .asSequence()
+    }
+    .filter { it.extension == "jar" }
+    .forEach {
+      it.copyTo(target.resolve(it.name), overwrite = true)
+    }
